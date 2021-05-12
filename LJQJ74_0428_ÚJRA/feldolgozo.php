b8a2293ba19cfe5c58703e89a9bb1053b8134a44
RepBala98 <balazs.repei@gmail.com>
2021-05-12 09:08:24 +0200

    0428 ujra

---
+++ LJQJ74_0428_ÚJRA/feldolgozo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+
+  <title>Feldolgozó php</title>
+ </head>
+ <body>
+
+ <font style="font-size:16pt">
+
+ <h2>HTML5 űrlap</h2>
+
+	<?php
+
+	echo ("<strong>Név:</strong>" . $_POST['nev'] . "<br><br>");
+
+	echo ("<strong>PIN kód:</strong>" . $_POST['kod'] . "<br><br>");
+
+	echo ("<strong>Gyümölcs:</strong>" . $_POST['gyumolcs'] . "<br><br>");
+
+	echo ("<strong>Életkor</strong>" . $_POST['eletkor'] . "<br><br>");
+
+	echo ("<strong>Lábméret:</strong>" . $_POST['labmeret'] . "<br><br>");
+
+	echo ("<strong>Önbizalom:</strong>" . $_POST['onbizalom'] . "<br><br>");
+
+	?>
+	<a href="Űrlap_KA.html">vissza az űrlapra...</a>
+  
+ </body>
+</html>
---
+++ LJQJ74_0428_ÚJRA/feldolgozo1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+
+  <title>Űrlap vezérlése új elemekkel</title>
+ </head>
+ <body>
+ <font style="font-size:18pt">
+
+ <h2>Űrlap vezérlése új elemekkel </h2>
+
+ 	<?php
+	print ("Név: " .$_Post['nev'] . "<br>");
+	print ("Pin kód: " .$_Post['kod'] . "<br>");
+
+	?>
+
+<a href="urlap3.html">vissza az űrlapra...</a>
+
+
+</font>
+  
+ </body>
+</html>
